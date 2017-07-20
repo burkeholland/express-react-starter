21d2f61e01e507f7bc53a8125f24c74d64726808
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2017-07-20 11:58:01 -0500

    modify project structure

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+
+const app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api', index);
+app.get('*', (req, res) => {
+    res.sendFile('build/index.html', {root: root});
+});
+
+module.exports = app;
