8ba413419b304772edd836b50e8a3edd6771b307
Burke Holland <burkeholland@gmail.com>
2022-05-12 12:29:54 -0500

    Fixes why there is  error mv build server ? #8

---
+++ copy.js
@@ -0,0 +1,19 @@
+// This is a simple script to copy the server folder to the "build" directory
+const { promises: fs } = require("fs");
+const path = require("path");
+
+async function copyDir(src, dest) {
+  await fs.mkdir(dest, { recursive: true });
+  let entries = await fs.readdir(src, { withFileTypes: true });
+
+  for (let entry of entries) {
+    let srcPath = path.join(src, entry.name);
+    let destPath = path.join(dest, entry.name);
+
+    entry.isDirectory()
+      ? await copyDir(srcPath, destPath)
+      : await fs.copyFile(srcPath, destPath);
+  }
+}
+
+copyDir("build", "server/build");
